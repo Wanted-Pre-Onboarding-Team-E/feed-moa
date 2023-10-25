4aab0eaa864ebd9581728a713fe2c304b35703ab
kangssu <gmltn0475@naver.com>
2023-10-25 13:53:09 +0900

    add: 프레임워크 라이브러리 설치 및 프로젝트 구조 완료

---
+++ server/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './feature/user/user.app.module';
+import { PostModule } from './feature/post/post.app.module';
+import { User } from './entity/user.entity';
+import { Post } from './entity/post.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User, Post],
+      synchronize: false, // 개발환경(DB 만들고 false로 변경하기)
+      logging: true,
+      keepConnectionAlive: true,
+    }),
+    PostModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
