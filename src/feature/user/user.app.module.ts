e321b305ddcde31df2dbe3076dc430a1a18d882e
dawwson <shines1427@gmail.com>
2023-10-27 05:15:53 +0900

    fix: 환경변수 읽어들일 수 있도록 config module import함

--- src/feature/user/user.app.module.ts
+++ src/feature/user/user.app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+
 import { User } from '../../entity/user.entity';
 import { UserRepository } from './repository/user.repository';
 import { UserController } from './user.controller';
@@ -9,6 +11,7 @@ import { UserLib } from './user.lib';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // TODO: configuration 파일 형식으로 정리 필요해보임
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
       // TODO: 환경변수로 분리
