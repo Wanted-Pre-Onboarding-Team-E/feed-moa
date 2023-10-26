26c0d46b1c9a06a33bd4893a9e30159382c030f7
sayapin1 <sayapinn1@gmail.com>
2023-10-27 04:02:43 +0900

    [#1]feat: statistic service 1차 작업 및 service에서 repository를 사용하기 위해 module에 import함

--- src/feature/statistics/statistics.module.ts
+++ src/feature/statistics/statistics.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StatisticsService } from './statistics.service';
 import { StatisticsController } from './statistics.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from '../../entity/post.entity';
+import { Hashtag } from '../../entity/hashtag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post, Hashtag])],
   controllers: [StatisticsController],
   providers: [StatisticsService],
 })
--- src/feature/statistics/statistics.service.ts
+++ src/feature/statistics/statistics.service.ts
@@ -1,26 +1,131 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStatisticDto } from './dto/create-statistic.dto';
-import { UpdateStatisticDto } from './dto/update-statistic.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  HttpException,
+  Injectable,
+  UnprocessableEntityException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Post } from '../../entity/post.entity';
+import { Hashtag } from '../../entity/hashtag.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class StatisticsService {
-  create(createStatisticDto: CreateStatisticDto) {
-    return 'This action adds a new statistic';
-  }
+  constructor(
+    @InjectRepository(Post) private postRepository: Repository<Post>,
+    @InjectRepository(Hashtag) private hashtagRepository: Repository<Hashtag>,
+  ) {}
 
-  findAll() {
-    return `This action returns all statistics`;
-  }
+  async getStatistics(query) {
+    console.log('hashtag:', query.hashtag);
+    console.log('value:', query.value);
+    console.log('type:', query.type);
 
-  findOne(id: number) {
-    return `This action returns a #${id} statistic`;
-  }
+    if (query.type === 'date') {
+      const startDate = new Date(query.start);
+      const endDate = new Date(query.end);
+      const thirtyDaysInMilliseconds = 30 * 24 * 60 * 60 * 1000;
+      if (endDate.getTime() - startDate.getTime() > thirtyDaysInMilliseconds) {
+        throw new UnprocessableEntityException(
+          '최대 조회 가능한 날짜는 30일입니다.',
+        );
+      }
+    }
 
-  update(id: number, updateStatisticDto: UpdateStatisticDto) {
-    return `This action updates a #${id} statistic`;
-  }
+    if (query.type === 'hour') {
+      const startHour = new Date(query.start);
+      const endHour = new Date(query.end);
+      const oneWeekInMilliseconds = 168 * 60 * 60 * 1000;
 
-  remove(id: number) {
-    return `This action removes a #${id} statistic`;
+      if (startHour.getTime() - endHour.getTime() > oneWeekInMilliseconds) {
+        throw new UnprocessableEntityException(
+          '최대 조회 가능한 시간은 일주일(168시간)입니다.',
+        );
+      }
+    }
+
+    if (query.value === 'count') {
+      const result = await this.postRepository
+        .createQueryBuilder('post')
+        .leftJoinAndSelect('post.hashtags', 'hashtag')
+        .select('DATE(post.created_at)', 'date')
+        .addSelect('COUNT(post.id)', 'count')
+        .where('hashtag.hashtag = :hashtag', { hashtag: query.hashtag })
+        .andWhere('post.created_at BETWEEN :startDate AND :endDate', {
+          startDate: query.start,
+          endDate: query.end,
+        })
+        .groupBy('DATE(post.created_at)')
+        .getRawMany();
+
+      if (result.length === 0) {
+        return 0;
+      }
+
+      return result;
+    }
+
+    if (query.value === 'view_count') {
+      const result = await this.postRepository
+        .createQueryBuilder('post')
+        .leftJoinAndSelect('post.hashtags', 'hashtag')
+        .select('DATE(post.created_at)', 'date')
+        .addSelect('SUM(post.view_count)', 'sum')
+        .where('hashtag.hashtag = :hashtag', { hashtag: query.hashtag })
+        .andWhere('post.created_at BETWEEN :startDate AND :endDate', {
+          startDate: query.start,
+          endDate: query.end,
+        })
+        .groupBy('DATE(post.created_at)')
+        .getRawMany();
+
+      if (result.length === 0) {
+        return 0;
+      }
+
+      return result;
+    }
+
+    if (query.value === 'like_count') {
+      const result = await this.postRepository
+        .createQueryBuilder('post')
+        .leftJoinAndSelect('post.hashtags', 'hashtag')
+        .select('DATE(post.created_at)', 'date')
+        .addSelect('SUM(post.like_count)', 'sum')
+        .where('hashtag.hashtag = :hashtag', { hashtag: query.hashtag })
+        .andWhere('post.created_at BETWEEN :startDate AND :endDate', {
+          startDate: query.start,
+          endDate: query.end,
+        })
+        .groupBy('DATE(post.created_at)')
+        .getRawMany();
+
+      if (result.length === 0) {
+        return 0;
+      }
+
+      return result;
+    }
+
+    if (query.value === 'share_count') {
+      const result = await this.postRepository
+        .createQueryBuilder('post')
+        .leftJoinAndSelect('post.hashtags', 'hashtag')
+        .select('DATE(post.created_at)', 'date')
+        .addSelect('SUM(post.share_count)', 'sum')
+        .where('hashtag.hashtag = :hashtag', { hashtag: query.hashtag })
+        .andWhere('post.created_at BETWEEN :startDate AND :endDate', {
+          startDate: query.start,
+          endDate: query.end,
+        })
+        .groupBy('DATE(post.created_at)')
+        .getRawMany();
+
+      if (result.length === 0) {
+        return 0;
+      }
+
+      return result;
+    }
   }
 }
