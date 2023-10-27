d20790d06ac411ce5de1f31ce9c11f97d0749932
sayapin1 <110008118+sayapin1@users.noreply.github.com>
2023-10-28 01:05:35 +0900

    [#1] feat: statistics에 쿼리가 없을 때 들어오는 기본값 추가

--- src/feature/statistics/dto/statistics.dto.ts
+++ src/feature/statistics/dto/statistics.dto.ts
@@ -1,7 +1,14 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class StatisticsDTO {
   hashtag: string;
+
   value: 'count' | 'view_count' | 'like_count' | 'share_count';
+
+  @IsNotEmpty()
   type: 'date' | 'hour';
+
   start: string;
+
   end: string;
 }
--- src/feature/statistics/statistics.controller.ts
+++ src/feature/statistics/statistics.controller.ts
@@ -8,6 +8,25 @@ export class StatisticsController {
 
   @Get()
   getStatistics(@Query() query: StatisticsDTO) {
+    if (!query.start) {
+      const oneWeekAgo = new Date();
+      oneWeekAgo.setDate(oneWeekAgo.getDate() - 6); // 6일 빼기
+      oneWeekAgo.setHours(oneWeekAgo.getHours() - 23); // 23시간 빼기
+      oneWeekAgo.setMinutes(oneWeekAgo.getMinutes() - 59);
+      query.start = oneWeekAgo.toISOString();
+    }
+
+    if (!query.end) {
+      query.end = new Date().toISOString();
+    }
+
+    if (!query.hashtag) {
+      query.hashtag = '태그';
+    }
+
+    if (!query.value) {
+      query.value = 'count';
+    }
     if (query.type === 'date') {
       return this.statisticsService.getStatisticsByDate(query);
     }
