ed927dfbcbb3cdc01400c53c2de67acd20c03a3a
sayapin1 <110008118+sayapin1@users.noreply.github.com>
2023-10-29 03:34:34 +0900

    [#1] fix: statistics.controller.ts 코드 수정
    1. query라는 변수명을 statsParams로 수정
    2. dto에서 value의 타입을 enum으로 주었기 때문에 controller에서도 valueType이라는 enum을 import해 사용하도록 수정

--- src/feature/statistics/statistics.controller.ts
+++ src/feature/statistics/statistics.controller.ts
@@ -1,37 +1,34 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { StatisticsService } from './statistics.service';
 import { StatisticsDTO } from './dto/statistics.dto';
+import { StatisticsValueType } from '../../enum/statisticsValueType.enum';
 
 @Controller('posts/statistics')
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
 
   @Get()
-  getStatistics(@Query() query: StatisticsDTO) {
-    if (!query.start) {
+  getStatistics(@Query() statsParams: StatisticsDTO) {
+    if (!statsParams.start) {
       const oneWeekAgo = new Date();
       oneWeekAgo.setDate(oneWeekAgo.getDate() - 6); // 6일 빼기
       oneWeekAgo.setHours(oneWeekAgo.getHours() - 23); // 23시간 빼기
       oneWeekAgo.setMinutes(oneWeekAgo.getMinutes() - 59);
-      query.start = oneWeekAgo.toISOString();
+      statsParams.start = oneWeekAgo.toISOString();
     }
 
-    if (!query.end) {
-      query.end = new Date().toISOString();
+    if (!statsParams.end) {
+      statsParams.end = new Date().toISOString();
     }
 
-    if (!query.hashtag) {
-      query.hashtag = '태그';
+    if (!statsParams.value) {
+      statsParams.value = StatisticsValueType.COUNT;
     }
-
-    if (!query.value) {
-      query.value = 'count';
+    if (statsParams.type === 'date') {
+      return this.statisticsService.getStatisticsByDate(statsParams);
     }
-    if (query.type === 'date') {
-      return this.statisticsService.getStatisticsByDate(query);
-    }
-    if (query.type === 'hour') {
-      return this.statisticsService.getStatisticsByHour(query);
+    if (statsParams.type === 'hour') {
+      return this.statisticsService.getStatisticsByHour(statsParams);
     }
   }
 }
