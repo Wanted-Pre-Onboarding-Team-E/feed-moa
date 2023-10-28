b2c3a53986281703db64488d3ff63a505ebdaf65
Lee seongwon <jsw6357@naver.com>
2023-10-28 20:34:25 +0900

    fix:게시물 좋아요 API 수정본

--- src/feature/post/post.app.module.ts
+++ src/feature/post/post.app.module.ts
@@ -4,9 +4,10 @@ import { PostService } from './post.service';
 import { PostRepository } from './repository/post.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from 'src/entity/post.entity';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post]), HttpModule],
   providers: [PostService, PostRepository],
   controllers: [PostController],
 })
--- src/feature/post/repository/post.repository.ts
+++ src/feature/post/repository/post.repository.ts
@@ -3,20 +3,48 @@ import { Repository } from 'typeorm';
 import { Post } from 'src/entity/post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PostType } from 'src/enum/postType.enum';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class PostRepository {
   constructor(
     @InjectRepository(Post)
     private postRepository: Repository<Post>,
+    private httpService: HttpService,
   ) {}
 
   async incrementPostLikeCount(type: PostType, postId: number) {
+    const snsEndpoint = this.getSNSEndpoints(type, postId);
+
+    const response = await firstValueFrom(this.httpService.get(snsEndpoint));
+
+    if (response.status === 200) {
+      await this.updateLikeCount(type, postId);
+    }
+  }
+
+  private async updateLikeCount(type: PostType, postId: number) {
     await this.postRepository
       .createQueryBuilder()
       .update(Post)
       .where({ id: postId, type })
       .set({ likeCount: () => 'likeCount + 1' })
       .execute();
+  }
+
+  private getSNSEndpoints(type: PostType, postId: number) {
+    switch (type) {
+      case PostType.FACEBOOK:
+        return `https://www.facebook.com/likes/${postId}`;
+      case PostType.TWITTER:
+        return `https://www.twitter.com/likes/${postId}`;
+      case PostType.INSTAGRAM:
+        return `https://www.instagram.com/likes/${postId}`;
+      case PostType.THREADS:
+        return `https://www.threads.net/likes/${postId}`;
+      default:
+        throw new Error('타입이 존재하지 않습니다.');
+    }
   }
 }
