f61f0efdbfa342723feaa1f9adf79c00720cf3d3
dawwson <shines1427@gmail.com>
2023-10-28 11:17:06 +0900

    fix: Auth 모듈에 providers 대신 User 모듈을 import 하도록 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategy/jwt.strategy';
 
-import { UserLib } from '../feature/user/user.lib';
-import { UserRepository } from '../feature/user/repository/user.repository';
-import { User } from '../entity/user.entity';
+import { UserModule } from '../feature/user/user.app.module';
 
 @Module({
-  imports: [PassportModule, TypeOrmModule.forFeature([User])],
-  providers: [AuthService, JwtStrategy, UserLib, UserRepository],
+  imports: [PassportModule, UserModule],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
