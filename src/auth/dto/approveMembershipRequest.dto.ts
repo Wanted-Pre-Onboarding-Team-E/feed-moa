142890d5617d7e557500f07f2d34a4f1d4403d5b
dawwson <shines1427@gmail.com>
2023-10-30 00:41:14 +0900

    refactor: DTO 필수값에 느낌표 추가

--- src/auth/dto/approveMembershipRequest.dto.ts
+++ src/auth/dto/approveMembershipRequest.dto.ts
@@ -2,9 +2,9 @@ import { IsNumberString, IsString, Length } from 'class-validator';
 
 export class ApproveMembershipRequestDto {
   @IsString()
-  username: string;
+  username!: string;
 
   @IsNumberString()
   @Length(6, 6)
-  authCode: string;
+  authCode!: string;
 }
