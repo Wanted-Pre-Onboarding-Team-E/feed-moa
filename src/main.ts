96bbfc396845c8a3b0885cff04933f89987a9072
dawwson <shines1427@gmail.com>
2023-10-29 23:25:23 +0900

    add: PostController에 JwtAuthGuard 설정

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.use(cookieParser());
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT);
-  console.log(process.env.PORT);
 }
 
 bootstrap();
