d721cdc72a478742c4b85e159855bca3aa595bdf
dawwson <shines1427@gmail.com>
2023-10-29 22:51:28 +0900

    fix: 중복된 선언문들 수정

--- src/feature/user/user.app.module.ts
+++ src/feature/user/user.app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from '../../entity/user.entity';
+import { AuthCode } from '../../entity/authCode.entity';
+
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserLib } from './user.lib';
-
-import { User } from '../../entity/user.entity';
-import { AuthCode } from '../../entity/authCode.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, AuthCode])],
